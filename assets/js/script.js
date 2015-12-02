724ba20a1920bdf55e6d09a79f64477f808b1b02
Kimberly Bourque <kbourque@berkeley.edu>
2015-12-02 03:48:06 -0800

    added some more jquery functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,43 +15,23 @@ $(document).ready(function() {
 
 /* Hovering over buttons in intro page. */
 
-	$('#mainicon1').hover(
+	var hovericon = function (pic, text) {
+		$(pic).hover(
 
 		function() {
-			$('#aboutme').fadeIn();
+			$(text).fadeIn();
 			$(this).css('opacity', '0.97');
 		},
 
 		function() {
-			$('#aboutme').fadeOut();
+			$(text).fadeOut();
 			$(this).css('opacity', '1');
 		});
+	}
 
-	$('#mainicon2').hover(
-
-		
-		function() {
-			$('#towork').fadeIn();
-			$(this).css('opacity', '0.97');
-		},
-
-		function() {
-			$('#towork').fadeOut();
-			$(this).css('opacity', '1');
-		});
-
-	$('#mainicon3').hover(
-
-		
-		function() {
-			$('#at').fadeIn();
-			$(this).css('opacity', '0.97');
-		},
-
-		function() {
-			$('#at').fadeOut();
-			$(this).css('opacity', '1');
-		});
+	hovericon('#mainicon1', '#aboutme');
+	hovericon('#mainicon2', '#towork');
+	hovericon('#mainicon3', '#at');
 
 /* Clicking buttons brings to respective div. */
 
@@ -70,19 +50,40 @@ $(document).ready(function() {
 		}
 	);
 
-/** SKILLS SECTION **/
+/** WORK SECTION **/
 
-	$('.progskills').hover(
-		function() {
-			$('.progskills').animate({
-				"-webkit-box-shadow": "32px 22px 5px 0px rgba(0,0,0,0.75)",
-				"-moz-box-shadow": "32px 22px 5px 0px rgba(0,0,0,0.75)",
-				"box-shadow": "32px 22px 5px 0px rgba(0,0,0,0.75)",
-				"height": "500"
-			}, 5000)
-		},
-		$(this).hide()
-		);
+var showtext = function (pic,text) {
+	$(pic).hover(
+
+	function(){
+		$(text).fadeTo(800, 1, function() {});
+  	},
+  	function(){
+  		$(text).fadeTo(800, 0, function() {});
+  	}
+
+	);
+}
+
+showtext('#gitletpic', '#gittext');
+showtext('#redimg', '#redtext');
+showtext('#dbimg', '#dbtext');
+showtext('#kband', '#kbtext');
+
+/** SKILLS SECTION **/
+	
+	/* COMMENTED CODE WILL IMPLEMENT LATER: LI SECTION RESPONSE WHEN YOU HOVER */
+
+	// $('.progskills').hover(
+	// 	function() {
+	// 		$(this).animate({
+	// 			// -webkit-box-shadow: 32px 22px 5px 0px rgba(0,0,0,0.75),
+	// 			// -moz-box-shadow: 32px 22px 5px 0px rgba(0,0,0,0.75),
+	// 			"box-shadow": 32px 22px 5px 0px rgba(0,0,0,0.75),
+	// 		}, 5000)
+	// 	},
+	// 	$(this).hide()
+	// 	);
 
 
 });
